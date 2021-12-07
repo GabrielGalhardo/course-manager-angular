b0845bab6daae60c705878ae000a3ae33bf21f1f
GabrielGalhardo <galhardogabriel843@gmail.com>
2021-12-07 06:49:06 -0300

    Configuraçoes + inicio ao componente Course

--- course-manager/src/app/app.module.ts
+++ course-manager/src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses/courses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ course-manager/src/app/courses/course.ts
@@ -0,0 +1,19 @@
+export class Course {
+  id: number;
+  name: string;
+  imageUrl: string;
+  price: number;
+  code: string;
+  duration: number;
+  rating: number;
+
+  constructor(id = 0, name = '', imageUrl = '', price = 0, code = '', duration = 0, rating = 0 ) {
+    this.id = id;
+    this.name = name;
+    this.imageUrl = imageUrl;
+    this.price = price;
+    this.code = code;
+    this.duration = duration;
+    this.rating = rating;
+   }
+}
