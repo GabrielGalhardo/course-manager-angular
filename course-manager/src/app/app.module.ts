17922ac6b72bda785140f60ccbb5431217e3af74
GabrielGalhardo <galhardogabriel843@gmail.com>
2021-12-13 07:23:11 -0300

    Rotas + Mascara em data

--- course-manager/src/app/app.module.ts
+++ course-manager/src/app/app.module.ts
@@ -1,19 +1,41 @@
+import { ReplacePipe } from './pipe/replace.pipe';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses/courses.component';
 import { StarComponent } from './star/star.component';
-
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { RouterModule } from '@angular/router';
+import { Error404Component } from './error-404/error-404.component';
+import { CourseInfoComponent } from './courses/course-info.component';
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponent,
+    CourseInfoComponent,
     StarComponent,
+    ReplacePipe,
+    NavBarComponent,
+    Error404Component,
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+        {
+          path: '', redirectTo: 'courses', pathMatch: 'full'
+        },
+        {
+          path: 'courses', component: CoursesComponent
+        },
+        {
+          path: 'courses-info/:id', component: CourseInfoComponent
+        },
+        {
+          path: '**', component: Error404Component
+        },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- course-manager/src/app/courses/courses.component.ts
+++ course-manager/src/app/courses/courses.component.ts
@@ -4,16 +4,26 @@ import { Course } from './course';
 import  CourseService  from './course.service';
 
 @Component({
-  selector: 'app-course-list',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.css']
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Course[] = [];
+filtredCourses: Course[] = [];
+  _courses: Course[] = [];
+
+  _filterBy: string = "";
   constructor(private courseService: CourseService) {}
   ngOnInit(): void {
-    this.courses = this.courseService.retriveAll();
+    this._courses = this.courseService.retriveAll();
+    this.filtredCourses = this._courses
+  }
+  set filter(value: string){
+    this._filterBy = value;
+    this.filtredCourses = this._courses.filter((course: Course) => course.name.toLocaleLowerCase().indexOf(this._filterBy.toLocaleLowerCase())> -1)
+  }
+  get filter(){
+    return this._filterBy
   }
 
 }
